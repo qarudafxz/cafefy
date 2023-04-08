8814f53f1bd9baacecc556f56a65a57c79ade52e
qaruda <70809588+francistinao@users.noreply.github.com>
2023-04-08 19:32:39 +0800

    ⚙️ fixed helper getName

--- api/helpers/getName.js
+++ api/helpers/getName.js
@@ -5,7 +5,7 @@ export const getName = async (req, res) => {
 		const user = await UserModel.findById(req.params.id);
 		if (!user) res.status(404).json({ message: "User not found" });
 
-		res.status(200).json(user.firstName, { message: "User found" });
+		res.json(user.firstName);
 	} catch (err) {
 		return res.status(401).json({ err, message: "Something went wrong" });
 	}
