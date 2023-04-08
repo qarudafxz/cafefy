2630bb301e9135cf2fb084960ac5e4b87cb8c631
qaruda <70809588+francistinao@users.noreply.github.com>
2023-04-08 19:42:58 +0800

    📚 added components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import { RxHamburgerMenu } from 'react-icons/rx';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <RxHamburgerMenu />
+    </nav>
+  )
+}
+
+export default Navbar
