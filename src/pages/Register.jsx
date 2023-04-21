f244d420b90d5a6778c662ed5b9fea5d54309955
qaruda <70809588+francistinao@users.noreply.github.com>
2023-04-22 00:01:15 +0800

    added and enhance authentication and authorization

---
+++ src/pages/Register.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+const Register = () => {
+  return (
+    <div className="xxxsm:m-xxxsm">
+      <Navbar />
+    </div>
+  )
+}
+
+export default Register
