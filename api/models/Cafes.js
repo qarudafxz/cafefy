db06f01c278a9f48eccb96ad5953d6533e3448cb
qaruda <70809588+francistinao@users.noreply.github.com>
2023-04-22 23:00:25 +0800

    can view cafe

--- api/models/Cafes.js
+++ api/models/Cafes.js
@@ -6,6 +6,7 @@ const CafeSchema = new mongoose.Schema({
 	name: { type: String, required: true },
 	desc: { type: String, required: true },
 	address: { type: String, required: true },
+	logo: { type: String, required: true },
 	image: { type: String, required: true },
 	averageRate: { type: Number, default: 0 },
 	numberOfRaters: { type: Number, default: 0 },
