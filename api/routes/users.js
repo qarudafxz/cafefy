297a88cec6448b874ff2db32330a3ddcc45dd1d9
qaruda <70809588+francistinao@users.noreply.github.com>
2023-04-27 17:15:58 +0800

    update

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getName } from "../helpers/getName.js";
-import { getUserById } from "../controllers/getUserById.js";
+import { getUserById } from "../helpers/getUserByID.js";
 import { editProfile } from "../controllers/editProfile.js";
 
 const router = express.Router();
