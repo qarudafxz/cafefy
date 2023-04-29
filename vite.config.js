dcc04cb620431d901270eecc2b3fc4c4c8bafb56
qaruda <70809588+francistinao@users.noreply.github.com>
2023-04-29 13:57:36 +0800

    added pwa

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
 
+// added pwa
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
