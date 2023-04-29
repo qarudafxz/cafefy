9db9e1fa45717bd715d4f29a58f204ef56ac060e
qaruda <70809588+francistinao@users.noreply.github.com>
2023-04-29 18:20:11 +0800

    added pwa again

--- vite.config.js
+++ vite.config.js
@@ -7,38 +7,38 @@ import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
 	plugins: [
 		react(),
-		// VitePWA({
-		// 	registerType: "autoUpdate",
-		// 	devOptions: {
-		// 		enabled: false,
-		// 	},
-		// 	includeAssets: ["android.png"],
-		// 	manifest: {
-		// 		name: "Cafefy",
-		// 		short_name: "Cafefy",
-		// 		description: "Cafe-Rating App",
-		// 		theme_color: "#131313",
-		// 		background_color: "#131313",
-		// 		start_url: "/",
-		// 		icons: [
-		// 			{
-		// 				src: "/android-launchericon-96-96.png",
-		// 				sizes: "96x96",
-		// 				type: "image/png",
-		// 			},
-		// 			{
-		// 				src: "/android-launchericon-192-192.png",
-		// 				sizes: "192x192",
-		// 				type: "image/png",
-		// 			},
-		// 			{
-		// 				src: "/android-launchericon-512-512.png",
-		// 				sizes: "512x512",
-		// 				type: "image/png",
-		// 			},
-		// 		],
-		// 		display: "standalone",
-		// 	},
-		// }),
+		VitePWA({
+			registerType: "autoUpdate",
+			devOptions: {
+				enabled: false,
+			},
+			includeAssets: ["android.png"],
+			manifest: {
+				name: "Cafefy",
+				short_name: "Cafefy",
+				description: "Cafe-Rating App",
+				theme_color: "#131313",
+				background_color: "#131313",
+				start_url: "/",
+				icons: [
+					{
+						src: "/android-launchericon-96-96.png",
+						sizes: "96x96",
+						type: "image/png",
+					},
+					{
+						src: "/android-launchericon-192-192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "/android-launchericon-512-512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+				],
+				display: "standalone",
+			},
+		}),
 	],
 });
